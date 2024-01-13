fe3af628e9f0e9b66517a6c451c912f9da90f2d1
Infinity <85234537+guptaanant682@users.noreply.github.com>
2024-01-13 22:55:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("This is nodejs tutorial for me");
+
+// process.exit(0); // This process has a lot of method but we are using exit now. By default the exit code is 0 for successful exit 
+// process.exit(1); // it crashes our application
